1bad53d7cbcb993199e77664bade1fd321fe6352
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-06-03 12:45:21 +1000

    Add initial AboutPage

---
+++ src/pages/about.jsx
@@ -0,0 +1,21 @@
+function AboutPage() {
+	return (
+		<>
+			<h1>About Nook Books</h1>
+			<p>
+				Nook Books is about creating a welcoming cozy space for all
+				bookworms to browse books to their hearts content.
+			</p>
+
+			<p>
+				I wanted to make browsing and exploring different genres
+				effortless and harken back to the days when we trailed through
+				the library shelves. When we judged a book by its cover and not
+				its rating. Because I believe that reading is a pleasure, never
+				a guilty one.
+			</p>
+		</>
+	);
+}
+
+export default AboutPage;
