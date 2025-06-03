87c8e543393057f52d346680631b4912794219a0
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-06-04 09:31:03 +1000

    Create HeroSection component

---
+++ src/components/common/placement/HeroSection.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+function HeroSection({ title, subtitle, button }) {
+	return (
+		<div className="flex flex-col items-center px-40 shadow-[0_0_5px_10px_rgba(0,0,0,0.2)] bg-secondary/20 shadow-pink-50 py-15 text-center gap-9">
+			<h1 className="flex flex-col items-center gap-3 font-heading">
+				<div className="pl-10 text-5xl">{title}</div>
+				<div className="flex">
+					<Image
+						className="relative w-auto h-17 left-4 rotate-6"
+						src="/logo.jpg"
+						alt="Nook Books' logo"
+						width="1372"
+						height="3982"
+					/>
+					<span className="-mr-10 text-7xl font-brand">
+						Nook Books
+					</span>
+				</div>
+			</h1>
+			<div>{subtitle}</div>
+			{button && <button>{button}</button>}
+		</div>
+	);
+}
+
+export default HeroSection;
