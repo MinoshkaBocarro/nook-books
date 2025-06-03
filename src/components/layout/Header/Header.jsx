82944d6a07f9d12f8c82fd2b3527f53066d6d3eb
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-06-03 20:28:01 +1000

    Create Header component

---
+++ src/components/layout/Header/Header.jsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+function Header() {
+	return (
+		<>
+			(
+			<header>
+				<div>
+					<Link href="/">Nook Books</Link>
+				</div>
+				<nav>
+					<ul>
+						<li>
+							<Link href="/about">About</Link>
+						</li>
+						<li>
+							<Link href="/genres">Genres</Link>
+						</li>
+						<li>
+							<Link href="/contact">Contact</Link>
+						</li>
+					</ul>
+				</nav>
+			</header>
+			)
+		</>
+	);
+}
+
+export default Header;
