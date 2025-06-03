649dc53541e68989fe2effef73e32f3531f9c25e
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-06-04 07:02:27 +1000

    Style Footer

--- src/components/layout/Footer/Footer.jsx
+++ src/components/layout/Footer/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
 	};
 
 	return (
-		<footer>
+		<footer className="p-2 text-center bg-teal-400 shadow-[0_-3px_6px_-1px_rgba(0,0,0,0.2)]">
 			<div>&copy; {getCurrentYear()} Nook Books</div>
 		</footer>
 	);
