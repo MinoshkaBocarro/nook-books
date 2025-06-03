1706b4cd614a7b7a272ffca853c43721144cf90c
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-06-03 11:21:34 +1000

    Setup file architecture and config for base site

---
+++ next.config.js
@@ -0,0 +1,26 @@
+const {
+	PHASE_DEVELOPMENT_SERVER,
+	PHASE_PRODUCTION_BUILD,
+} = require("next/constants");
+
+module.exports = (phase) => {
+	// Setting Phase
+	const isDev = phase === PHASE_DEVELOPMENT_SERVER;
+	const isProd = phase === PHASE_PRODUCTION_BUILD;
+	console.log(`isDev:${isDev}  isProd:${isProd}`);
+
+	const reactStrictMode = true;
+
+	const env = {
+		SERVER_NAME: (() => {
+			if (isDev) return "http://localhost:3000/";
+			if (isProd) return "https://nookbooks.vercel.app/";
+		})(),
+	};
+
+	// Next.config returns an object
+	return {
+		reactStrictMode,
+		env,
+	};
+};
