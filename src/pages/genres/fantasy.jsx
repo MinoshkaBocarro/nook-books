f81de77ae57e83878fe30d0403b02df603ca0a2f
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-06-03 18:45:21 +1000

    Trial api request on FantasyPage

---
+++ src/pages/genres/fantasy.jsx
@@ -0,0 +1,26 @@
+function FantasyPage({ books }) {
+	return (
+		<div>
+			<p>This is a fantasy page</p>
+			{books.length > 0 &&
+				books.map((book) => <p key={book.isbn}>{book.title}</p>)}
+		</div>
+	);
+}
+
+export const getStaticProps = async () => {
+	const response = await fetch(`${process.env.SERVER_NAME}/api/books`);
+	const data = await response.json();
+
+	if (!response.ok) {
+		throw new Error(
+			`Failed to fetch books - Error ${response.status}: ${data.message}`
+		);
+	}
+	return {
+		props: { books: data },
+		revalidate: 60 * 60,
+	};
+};
+
+export default FantasyPage;
