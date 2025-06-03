aa0458dd08e39156fc2bca295157c11f51684aef
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-06-04 07:03:30 +1000

    Style Layout

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -4,9 +4,11 @@ import Header from "./Header";
 
 function Layout({ children }) {
 	return (
-		<div>
+		<div className="box-border flex flex-col min-h-screen font-primary bg-background">
 			<Header />
-			<main>{children}</main>
+			<main className="container px-20 mx-auto my-10 grow">
+				{children}
+			</main>
 			<Footer />
 		</div>
 	);
