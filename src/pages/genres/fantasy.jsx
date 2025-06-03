8631a92fcafc383705a19daed1d760cff229f7d1
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-06-03 19:24:04 +1000

    Deliver blank FantasyPage due to deployment error

--- src/pages/genres/fantasy.jsx
+++ src/pages/genres/fantasy.jsx
@@ -2,29 +2,29 @@ function FantasyPage({ books }) {
 	return (
 		<div>
 			<p>This is a fantasy page</p>
-			{books.length > 0 &&
-				books.map((book) => <p key={book.isbn}>{book.title}</p>)}
+			{/* {books.length > 0 &&
+				books.map((book) => <p key={book.isbn}>{book.title}</p>)} */}
 		</div>
 	);
 }
 
-export const getStaticProps = async () => {
-	const response = await fetch(
-		`${process.env.SERVER_NAME}/api/books/genres/fantasy/`
-	);
-	const data = await response.json();
+// export const getStaticProps = async () => {
+// 	const response = await fetch(
+// 		`${process.env.SERVER_NAME}/api/books/genres/fantasy/`
+// 	);
+// 	const data = await response.json();
 
-	console.log(data);
+// 	console.log(data);
 
-	if (!response.ok) {
-		throw new Error(
-			`Failed to fetch books - Error ${response.status}: ${data.message}`
-		);
-	}
-	return {
-		props: { books: data },
-		revalidate: 60 * 60,
-	};
-};
+// 	if (!response.ok) {
+// 		throw new Error(
+// 			`Failed to fetch books - Error ${response.status}: ${data.message}`
+// 		);
+// 	}
+// 	return {
+// 		props: { books: data },
+// 		revalidate: 60 * 60,
+// 	};
+// };
 
 export default FantasyPage;
