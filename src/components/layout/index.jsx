990a1d2df500561366f05bd72e8f52f4d47afe78
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-06-04 11:09:09 +1000

    Update and style AboutPage

--- src/components/layout/index.jsx
+++ src/components/layout/index.jsx
@@ -6,7 +6,7 @@ function Layout({ children }) {
 	return (
 		<div className="box-border flex flex-col min-h-screen font-primary bg-background">
 			<Header />
-			<main className="mx-auto h-[calc(100vh-192px)] my-10 flex flex-col text-2xl">
+			<main className="flex flex-col mx-auto my-10 text-2xl grow">
 				{children}
 			</main>
 			<Footer />
--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,19 +1,38 @@
+import Image from "next/image";
+
+// components
+import HeroSection from "@/components/common/placement/HeroSection";
+
 function AboutPage() {
 	return (
 		<>
-			<h1>About Nook Books</h1>
-			<p>
-				Nook Books is about creating a welcoming cozy space for all
-				bookworms to browse books to their hearts content.
-			</p>
-
-			<p>
-				I wanted to make browsing and exploring different genres
-				effortless and harken back to the days when we trailed through
-				the library shelves. When we judged a book by its cover and not
-				its rating. Because I believe that reading is a pleasure, never
-				a guilty one.
-			</p>
+			<div
+				className="w-[100vw] h-[130px] bg-repeat-x bg-contain"
+				style={{ backgroundImage: "url('/hero-edge-top.jpg')" }}
+			></div>
+			<HeroSection
+				title="About"
+				subtitle="Creating a welcoming, cozy space for all
+				bookworms to browse books to their hearts' content."
+			/>
+			<div className="flex justify-center w-full">
+				<div className="flex items-center justify-center gap-10 px-20 pt-20 max-w-300 min-w-215">
+					<Image
+						src="/about-page-bookshelf.jpg"
+						alt="A cute cartoon bookshelf"
+						width={2000}
+						height={2000}
+						className="object-scale-down w-auto h-68"
+					/>
+					<p className="container text-center padding">
+						I wanted to make browsing and exploring different genres
+						effortless and harken back to the days when we slowly
+						trailed along the library shelves. When we judged a book
+						by its cover and not its rating. Because I believe that
+						reading is a pleasure, never a guilty one.
+					</p>
+				</div>
+			</div>
 		</>
 	);
 }
