a11ed4883606d6a690092b39ab7095f0d26db472
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2025-06-04 11:15:46 +1000

    Refactor Layout component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -11,22 +11,16 @@ function HomePage() {
 					content="Find your next great read with Nook Books - the best site for exploring book genres and discovering new books. Browse suggestions from all the genres from fiction with epic fantasy, sci-fi, heartwarming romance, thrilling mystery, historical-fiction to thought provoking non-fiction biographies!"
 				/>
 			</Head>
-			<div className="flex flex-col">
-				<div
-					className="w-[100vw] h-[130px] bg-repeat-x bg-contain"
-					style={{ backgroundImage: "url('/hero-edge-top.jpg')" }}
-				></div>
-				<HeroSection
-					title="Welcome to"
-					subtitle="A world of stories, knowledge and imagination awaits. Discover your next favourite read!"
-					buttonContent="Start Your Journey"
-					buttonPath="/genres"
-				></HeroSection>
-				<div
-					className="w-[100vw] h-[130px] bg-repeat-x bg-contain"
-					style={{ backgroundImage: "url('/hero-edge-bottom.jpg')" }}
-				></div>
-			</div>
+			<HeroSection
+				title="Welcome to"
+				subtitle="A world of stories, knowledge and imagination awaits. Discover your next favourite read!"
+				buttonContent="Start Your Journey"
+				buttonPath="/genres"
+			></HeroSection>
+			<div
+				className="w-[100vw] h-[130px] bg-repeat-x bg-contain"
+				style={{ backgroundImage: "url('/hero-edge-bottom.jpg')" }}
+			></div>
 		</>
 	);
 }
